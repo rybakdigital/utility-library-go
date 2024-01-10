7d87a4a098b9b8c16c5f0895b9b1d7c9edb4d96e
krisrybak <kris.rybak@hotmail.co.uk>
2024-01-10 15:01:42 +0000

    Updates Hash package and adds new methods to support base64 and special chars

--- hash/hash.go
+++ hash/hash.go
@@ -6,20 +6,49 @@ import (
 )
 
 const base62 = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+const base64 = "-_"
+const baseSpecial = "!?@$#%^&*()+="
 
 // GenerateHash generates random hash from base62 (alphanumeric) with a given length
 func GenerateHash(length int) string {
-	// Add random seed
-	rand.NewSource(time.Now().UnixNano())
+	return generate("base62", length)
+}
 
+// GenerateBase64 generates random hash from base64 (alphanumeric plus -_) with a given length
+func GenerateBase64(length int) string {
+	return generate("base64", length)
+}
+
+// GenerateHashSpecial generates random hash from baseSpecial (base62, base64 and baseSpecial) with a given length
+func GenerateBaseSpecial(length int) string {
+	return generate("baseSpecial", length)
+}
+
+func generate(base string, length int) string {
 	hash := make([]byte, length)
 
 	// Add random characters to hash
 	for i := range hash {
-		hash[i] = base62[rand.Int63()%int64(len(base62))]
+		hash[i] = getHashChar(base)
 	}
 
 	return string(hash)
+}
+
+func getHashChar(baseType string) byte {
+	// Add random seed
+	rand.NewSource(time.Now().UnixNano())
+
+	// Select base to use
+	base := base62
+	switch baseType {
+	case "base64":
+		base += base64
+	case "baseSpecial":
+		base += base64 + baseSpecial
+	}
+
+	return base[rand.Int63()%int64(len(base))]
 }
 
 // Hash16 Helper function to quickly generate hash of 16 character length
--- hash/hash_test.go
+++ hash/hash_test.go
@@ -2,6 +2,7 @@ package hash
 
 import (
 	"fmt"
+	"slices"
 	"testing"
 
 	"github.com/go-playground/assert/v2"
@@ -20,12 +21,94 @@ func TestGenerateHash(t *testing.T) {
 	}
 }
 
+func TestGenerateBase64(t *testing.T) {
+	sizes := []int{3, 5, 10}
+
+	for _, length := range sizes {
+		hash := GenerateBase64(length)
+		assert.Equal(t, length, len(hash))
+
+		// Making sure random hash has been generated
+		secondHash := GenerateBase64(length)
+		assert.NotEqual(t, hash, secondHash)
+	}
+}
+
+func TestGenerateBaseSpecial(t *testing.T) {
+	sizes := []int{3, 5, 10}
+
+	for _, length := range sizes {
+		hash := GenerateBaseSpecial(length)
+		assert.Equal(t, length, len(hash))
+
+		// Making sure random hash has been generated
+		secondHash := GenerateBaseSpecial(length)
+		assert.NotEqual(t, hash, secondHash)
+	}
+}
+
 func TestHash16(t *testing.T) {
 	assert.Equal(t, len(Hash16()), 16)
 }
 
 func TestHash32(t *testing.T) {
 	assert.Equal(t, len(Hash32()), 32)
+}
+
+func TestGetHashCharBase62(t *testing.T) {
+	// Let's check the scenario number of times, to assure better randomness
+	for i := 0; i < 100; i++ {
+		// Generate random Base62 character
+		char := getHashChar("")
+
+		if !slices.Contains([]byte(base62), char) {
+			t.Errorf("Failed to assert that character %s is present in base %s", string(char), base62)
+		}
+	}
+}
+
+func TestGetHashCharBase64(t *testing.T) {
+	// Let's check the scenario number of times, to assure better randomness
+	for i := 0; i < 100; i++ {
+		// Generate random base64 character
+		char := getHashChar("base64")
+
+		if !slices.Contains([]byte(base62+base64), char) {
+			t.Errorf("Failed to assert that character %s is present in base %s", string(char), base62+base64)
+		}
+	}
+}
+
+func TestGetHashCharBaseSpecial(t *testing.T) {
+	// Let's check the scenario number of times, to assure better randomness
+	for i := 0; i < 100; i++ {
+		// Generate random baseSpecial character
+		char := getHashChar("baseSpecial")
+
+		if !slices.Contains([]byte(base62+base64+baseSpecial), char) {
+			t.Errorf("Failed to assert that character %s is present in base %s", string(char), base62+base64+baseSpecial)
+		}
+	}
+}
+
+func TestAbleToGenerateBaseSpecialChar(t *testing.T) {
+	// Making sure the attempt number is rather large
+	attempts := 10000
+
+	for i := 1; i <= attempts; i++ {
+		// Generate random baseSpecial character
+		char := getHashChar("baseSpecial")
+
+		// If random character is a special one break out and finish test
+		if slices.Contains([]byte(baseSpecial), char) {
+			break // break here
+		}
+
+		// We have tried many times and failed to generate special character
+		if i == attempts {
+			t.Errorf("Failed to generate character from baseSpecial, tried %d times", i)
+		}
+	}
 }
 
 func BenchmarkGenerateHash(b *testing.B) {
